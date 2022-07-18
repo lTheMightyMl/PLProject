de88e7de2e17b5707553af8e6412f7fcd0291f0b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-07-18 20:41:52 +0430

    add datatype to expressions

--- main.rkt
+++ main.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require (lib "eopl.ss" "eopl"))
 
 (require parser-tools/lex
          (prefix-in : parser-tools/lex-sre)
@@ -51,6 +52,40 @@
     (token-ID lexeme))
    (whitespace (simple-python-lexer input-port))))
 
+(define string->variable-name
+   (lambda (stx) #'stx))
+
+(define-datatype python-exp python-exp?
+  (pass)
+  (break)
+  (continue)
+  (number (num number?))
+  (identifier (id identifier?))
+  (assign (id string?) (val python-exp?))
+  (return-void)
+  (return-value (exp python-exp?))
+  (define-global (exp python-exp?))
+  (define-function-with-params (name python-exp?) (params list?) (body python-exp?))
+  (define-function-without-params (name python-exp?) (body python-exp?))
+  (if (conditions python-exp?) (body python-exp?) (else python-exp?))
+  (for (counter string?) (range python-exp?) (body python-exp?))
+  (or (arg1 boolean?) (arg2 boolean?))
+  (and (arg1 boolean?) (arg2 boolean?))
+  (not (arg boolean?))
+  (compare-eq (arg1 python-exp?) (arg2 python-exp?))
+  (compare-lt (arg1 python-exp?) (arg2 python-exp?))
+  (compare-gt (arg1 python-exp?) (arg2 python-exp?))
+  (add (arg1 python-exp?) (arg2 python-exp?))
+  (subtract (arg1 python-exp?) (arg2 python-exp?))
+  (power (arg1 python-exp?) (arg2 python-exp?))
+  (multiply (arg1 python-exp?) (arg2 python-exp?))
+  (divide (arg1 python-exp?) (arg2 python-exp?))
+  (plus (arg python-exp?))
+  (minus (arg python-exp?))
+  (get-index (id string?) (index python-exp?))
+  (call-function-with-no-argument (id string?))
+  (call-function-with-arguments (id string?) (args list?)))
+
 (define-tokens a (NUMBER ID))
 (define-empty-tokens b (EOF semicolon pass break continue equal-sign return global def rpar lpar colon empty-params comma if else for in or and not eq lt gt plus minus multiply divide power lbra rbra empty-args true false none empty-list))
 
@@ -62,140 +97,141 @@
    (tokens a b)
    (grammar
     (Program
-     ((Statements) (list `statements $1)))
+     ((Statements) $1))
     
     (Statements
-     ((Statement semicolon) (append $1))
-     ((Statements Statement semicolon) (list $1 $2)))
+     ((Statement semicolon) (list $1))
+     ((Statements Statement semicolon) (cons $2 $1)))
     
     (Statement
-     ((Compound_stmt) (list `a-compound-stmt $1))
-     ((Simple_stmt) (list `a-simple-stmt $1)))
+     ((Compound_stmt) $1)
+     ((Simple_stmt) $1))
     
     (Simple_stmt
-     ((Assignment) (list `a-assignment $1))
-     ((Global_stmt) (list `a-global-stmt $1))
-     ((Return_stmt) (list `a-return-stmt $1))
-     ((pass) (list `pass))
-     ((break) (list `break))
-     ((continue) (list `continue)))
+     ((Assignment) $1)
+     ((Global_stmt) $1)
+     ((Return_stmt) $1)
+     ((pass) (pass))
+     ((break) (break))
+     ((continue) (continue)))
 
     (Compound_stmt
-     ((Function_def) (list `a-function-def $1))
-     ((If_stmt) (list `a-if-stmt $1))
-     ((For_stmt) (list `a-for-stmt $1)))
+     ((Function_def) $1)
+     ((If_stmt) $1)
+     ((For_stmt) $1))
 
     (Assignment
-     ((ID equal-sign Expression) (list `assign $1 $3)))
+     ((ID equal-sign Expression) (assign $1 $3)))
 
     (Return_stmt
-     ((return) (list `return_void))
-     ((return Expression) (list `return_value $2)))
+     ((return) (return-void))
+     ((return Expression) (return-value $2)))
 
     (Global_stmt
-     ((global ID) (list `define-global $2)))
+     ((global ID) (define-global $2)))
 
     (Function_def
-     ((def ID lpar Params rpar colon Statements) (list `define-function-with-params $2 (list `params $4) $7))
-     ((def ID empty-params colon Statements) (list `define-function-without-params $2 $5)))
+     ((def ID lpar Params rpar colon Statements) (define-function-with-params $2 $4 $7))
+     ((def ID empty-params colon Statements) (define-function-without-params $2 $5)))
 
     (Params
-     ((Param_with_default) (append $1))
-     ((Params comma Param_with_default) (list $1 $3)))
+     ((Param_with_default) (list $1))
+     ((Params comma Param_with_default) (append $1 $3)))
 
     (Param_with_default
-     ((ID equal-sign Expression) (list `assign $1 $3)))
+     ((ID equal-sign Expression) (assign $1 $3)))
 
     (If_stmt
-     ((if Expression colon Statements Else_block) (list `if $2 $4 $5)))
+     ((if Expression colon Statements Else_block) (if $2 $4 $5)))
 
     (Else_block
-     ((else colon Statements) (list `an-else-block $3)))
+     ((else colon Statements) $3))
 
     (For_stmt
-     ((for ID in Expression colon Statements) (list `for $2 $4 $6)))
+     ((for ID in Expression colon Statements) (for $2 $4 $6)))
 
     (Expression
-     ((Disjunction) (list `a-disjunction $1)))
+     ((Disjunction) $1))
 
     (Disjunction
-     ((Conjunction) (list `a-conjunction $1))
-     ((Disjunction or Conjunction) (list `or $1 $3)))
+     ((Conjunction) $1)
+     ((Disjunction or Conjunction) (or $1 $3)))
 
     (Conjunction
-     ((Inversion) (list `an-inversion $1))
-     ((Conjunction and Inversion) (list `and $1 $3)))
+     ((Inversion) $1)
+     ((Conjunction and Inversion) (and $1 $3)))
 
     (Inversion
-     ((not Inversion) (list `not $2))
-     ((Comparison) (list `a-comparison $1)))
+     ((not Inversion) (not $2))
+     ((Comparison) $1))
 
     (Comparison
-     ((Eq_Sum) (list `an-eq-sum $1))
-     ((Lt_Sum) (list `a-lt-sum $1))
-     ((Gt_Sum) (list `a-gt-sum $1))
-     ((Sum) (list `a-sum $1)))
+     ((Eq_Sum) $1)
+     ((Lt_Sum) $1)
+     ((Gt_Sum) $1)
+     ((Sum) $1))
 
     (Eq_Sum
-     ((Sum eq Sum) (list `compare-eq $1 $3)))
+     ((Sum eq Sum) (compare-eq $1 $3)))
 
     (Lt_Sum
-     ((Sum lt Sum) (list `compare-lt $1 $3)))
+     ((Sum lt Sum) (compare-lt $1 $3)))
 
     (Gt_Sum
-     ((Sum gt Sum) (list `compare-gt $1 $3)))
+     ((Sum gt Sum) (compare-gt $1 $3)))
 
     (Sum
-     ((Sum plus Term) (list `add $1 $3))
-     ((Sum minus Term) (list `minus $1 $3))
-     ((Term) (list `a-term $1)))
+     ((Sum plus Term) (add $1 $3))
+     ((Sum minus Term) (subtract $1 $3))
+     ((Term) $1))
 
     (Term
-     ((Term multiply Factor) (list `multiply $1 $3))
-     ((Term divide Factor) (list `divide $1 $3))
-     ((Factor) (list `a-factor $1)))
+     ((Term multiply Factor) (multiply $1 $3))
+     ((Term divide Factor) (divide $1 $3))
+     ((Factor) $1))
 
     (Factor
-     ((plus Power) (list `plus $2))
-     ((minus Power) (list `minus $2))
-     ((Power) (list `a-power $1)))
+     ((plus Power) (plus $2))
+     ((minus Power) (minus $2))
+     ((Power) $1))
 
     (Power
-     ((Atom power Factor) (list `power $1 $3))
-     ((Primary) (list `a-primary $1)))
+     ((Atom power Factor) (power $1 $3))
+     ((Primary) $1))
 
     (Primary
-     ((Atom) (list `an-atom $1))
-     ((Primary lbra Expression rbra) (list `get-index $1 $3))
-     ((Primary empty-params) (list `call-function-with-no-argument $1))
-     ((Primary lpar Arguments rpar) (list `call-function-with-arguments $1 (list `arguments $3))))
+     ((Atom) $1)
+     ((Primary lbra Expression rbra) (get-index $1 $3))
+     ((Primary empty-params) (call-function-with-no-argument $1))
+     ((Primary lpar Arguments rpar) (call-function-with-arguments $1 $3)))
 
     (Arguments
-     ((Expression) (append $1))
-     ((Arguments comma Expression) (list $1 $3)))
+     ((Expression) (list $1))
+     ((Arguments comma Expression) (append $1 $3)))
 
     (Atom
-     ((ID) (list `an-id $1))
-     ((true) (list `true #t))
-     ((false) (list `false #f))
-     ((none) (list `none null))
-     ((NUMBER) (list `a-number $1))
-     ((List) (list `a-list $1)))
+     ((ID) (identifier (string->variable-name $1)))
+     ((true) #t)
+     ((false) #f)
+     ((none) null)
+     ((NUMBER) (number $1))
+     ((List) $1))
 
     (List
-     ((lbra Expressions rbra) (list `expressions $2))
-     ((empty-list) (list `empty-list `())))
+     ((lbra Expressions rbra) $2)
+     ((empty-list) `()))
 
     (Expressions
-     ((Expression) (append $1))
-     ((Expressions comma Expression) (list $1 $3)))
+     ((Expression) (list $1))
+     ((Expressions comma Expression) (append $1 $3)))
 
     )))
 
 ;test
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
 
-(define your-lexer (lex-this simple-python-lexer (open-input-string "a=1; b=2;")))
+(define your-lexer (lex-this simple-python-lexer (open-input-string "a=1; b=2; c=3; d=4;")))
 
 (simple-python-parser your-lexer)
+
 
