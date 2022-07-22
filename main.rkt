260798779ad5f1307c04ea7a31a9f6879f517a9e
MoNam97 <mohamad0namdar@gmail.com>
2022-07-22 13:10:26 +0430

    arithmetic operations

--- main.rkt
+++ main.rkt
@@ -16,6 +16,53 @@
   (cases python-exp exp
     (statements (stmts stmt)
                 (value-of (value-of stmt) env))
+    (compare-eq (arg1 arg2)
+                (let ([val1 (car (value-of arg1 env is-global))]
+                      [val2 (car (value-of arg2 env is-global))])
+                      (list (if (eqv? (expval->num val1) (expval->num val2))
+                        (bool-val #t) (bool-val #f)) env #f)))
+
+    (compare-lt (arg1 arg2)
+                (let ([val1 (car (value-of arg1 env is-global))]
+                      [val2 (car (value-of arg2 env is-global))])
+                      (list (if (< (expval->num val1) (expval->num val2))
+                        (bool-val #t) (bool-val #f)) env #f)))
+                        
+    (compare-gt (arg1 arg2)
+                (let ([val1 (car (value-of arg1 env is-global))]
+                      [val2 (car (value-of arg2 env is-global))])
+                      (list (if (> (expval->num val1) (expval->num val2))
+                        (bool-val #t) (bool-val #f)) env #f)))
+
+    (add  (arg1 arg2)
+          (let ([val1 (expval->num (car (value-of arg1 env is-global)))]
+                [val2 (expval->num (car (value-of arg2 env is-global)))])
+                (list (num-val (+ val1 val2)) env #f)))
+                
+    (subtract (arg1 arg2)
+              (let ([val1 (expval->num (car (value-of arg1 env is-global)))]
+                    [val2 (expval->num (car (value-of arg2 env is-global)))])
+                    (list (num-val (- val1 val2)) env #f)))
+    (power  (arg1 arg2)
+            (let ([val1 (expval->num (car (value-of arg1 env is-global)))]
+                  [val2 (expval->num (car (value-of arg2 env is-global)))])
+                  (list (num-val (expt val1 val2)) env #f)))
+    (multiply (arg1 arg2)
+              (let ([val1 (expval->num (car (value-of arg1 env is-global)))]
+                    [val2 (expval->num (car (value-of arg2 env is-global)))])
+                    (list (num-val (* val1 val2)) env #f)))
+    (divide (arg1 arg2)
+            (let ([val1 (expval->num (car (value-of arg1 env is-global)))]
+                  [val2 (expval->num (car (value-of arg2 env is-global)))])
+                  (list (num-val (/ val1 val2)) env #f)))
+
+    (plus (arg) (let ([val (car (value-of arg env is-global))])
+                  (list val env #f)))
+
+    (minus  (arg) (let ([val (expval->num (car (value-of arg env is-global)))])
+                  (list (num-val(- 0 val)) env #f)))
+    
+
     (else '())))
 
 ;test
